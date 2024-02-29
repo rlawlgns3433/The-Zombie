6a79b67c488915abe9fb431892fb134979dab4ef
조병민 <jsjbm@naver.com>
2024-02-29 15:37:50 +0900

    임시저장

---
+++ Scenes/SceneScore.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#include "SceneScore.h"
+
+SceneScore::SceneScore(SceneIds id)
+	:Scene(id)
+{
+}
+
+void SceneScore::Init()
+{
+	Scene::Init();
+}
+
+void SceneScore::Release()
+{
+	Scene::Release();
+}
+
+void SceneScore::Reset()
+{
+	Scene::Reset();
+}
+
+void SceneScore::Enter()
+{
+	Scene::Enter();
+}
+
+void SceneScore::Exit()
+{
+	Scene::Exit();
+}
+
+void SceneScore::Update(float dt)
+{
+	Scene::Update(dt);
+}
+
+void SceneScore::LateUpdate(float dt)
+{
+	Scene::LateUpdate(dt);
+}
+
+void SceneScore::FixedUpdate(float dt)
+{
+	Scene::FixedUpdate(dt);
+}
+
+void SceneScore::DebugUpdate(float dt)
+{
+	Scene::DebugUpdate(dt);
+}
+
+void SceneScore::Draw(sf::RenderWindow& window)
+{
+	Scene::Draw(window);
+}
+
+void SceneScore::DebugDraw(sf::RenderWindow& window)
+{
+	Scene::DebugDraw(window);
+}
---
+++ Scenes/SceneScore.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "TextGo.h"
+class SceneScore : public Scene
+{
+
+protected:
+	TextGo
+
+public:
+	SceneScore(SceneIds id);
+	~SceneScore() override = default;
+
+
+	void Init() override;
+	void Release() override;
+	void Reset() override;
+
+	void Enter() override;
+	void Exit() override;
+
+	void Update(float dt) override;
+	void LateUpdate(float dt) override;
+	void FixedUpdate(float dt) override;
+	void DebugUpdate(float dt) override;
+	void Draw(sf::RenderWindow& window) override;
+	void DebugDraw(sf::RenderWindow& window) override;
+};
+
