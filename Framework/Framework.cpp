8e460e74ed56881e475c89199e433a03e24a8eb5
조병민 <jsjbm@naver.com>
2024-03-03 21:49:13 +0900

    기본 해상도 복원

--- Framework/Framework.cpp
+++ Framework/Framework.cpp
@@ -13,7 +13,7 @@ void Framework::Init(int width, int height, const std::string& name)
 	window.create(sf::VideoMode(windowSize.x, windowSize.y), name);
 	//window.setFramerateLimit(15);
 	window.setMouseCursorVisible(false);
-	window.setSize({ 1280, 720 });
+	//window.setSize({ 1280, 720 });
 	window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width - window.getSize().x) / 2,
 		(sf::VideoMode::getDesktopMode().height - window.getSize().y) / 2 ));
 	
